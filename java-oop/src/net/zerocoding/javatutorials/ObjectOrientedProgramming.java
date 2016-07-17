f9894e611a651133738b23a37eaa9f3b44b2afc5
YoungsuPark <nice2seeu86@gmail.com>
2016-07-17 11:45:03 +0900

    2016.07.17   - Hello Git World2 프린트 메소드 추가.

--- java-oop/src/net/zerocoding/javatutorials/ObjectOrientedProgramming.java
+++ java-oop/src/net/zerocoding/javatutorials/ObjectOrientedProgramming.java
@@ -2,6 +2,7 @@ package net.zerocoding.javatutorials;
 
 public class ObjectOrientedProgramming {
 	public static void main(String[] args) {
-		System.out.println("Hello GIT World!!");
+		System.out.println("Hello GIT World1!!");
+		System.out.println("Hello GIT World2!!");
 	}
 }
